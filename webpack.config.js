8ad0f6b12f1bf368e9c47475eacae1a6009e6910
Tom Rutgers <trutgers01@hotmail.com>
2020-04-13 14:20:01 +0200

    Add externals to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,28 @@
 const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+const externals = [
+  {
+    name: `react`,
+    global: `React`,
+    assetDir: `umd`,
+    assetName: `react.production.min.js`,
+  },
+  {
+    name: `react-dom`,
+    global: `ReactDOM`,
+    assetDir: `umd`,
+    assetName: `react-dom.production.min.js`,
+  },
+  {
+    name: `netlify-cms-app`,
+    global: `NetlifyCmsApp`,
+    assetDir: `dist`,
+    assetName: `netlify-cms-app.js`,
+    sourceMap: `netlify-cms-app.js.map`,
+  },
+]
 
 module.exports = {
   
@@ -30,6 +53,32 @@ module.exports = {
       }
     ]
   },
+
+  externals: externals.map(({ name, global }) => {
+    return {
+      [name]: global,
+    }
+  }),
+
+  plugins: [
+    new CopyPlugin(
+      [].concat.apply(
+        [],
+        externals.map(({ name, assetName, sourceMap, assetDir }) =>
+          [
+            {
+              from: require.resolve(path.join(name, assetDir, assetName)),
+              to: assetName,
+            },
+            sourceMap && {
+              from: require.resolve(path.join(name, assetDir, sourceMap)),
+              to: sourceMap,
+            },
+          ].filter(item => item)
+        )
+      )
+    ),
+  ],
 
   optimization: { 
     minimize: true, 
