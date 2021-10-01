8e7a176ddd0b66424d8d194983ba123861a7c81f
Tom Rutgers <info@tomrutgers.nl>
2021-10-01 18:22:48 +0200

    Add Tailwind CSS

--- postcss.config.js
+++ postcss.config.js
@@ -5,6 +5,8 @@ module.exports = {
       browsers: '>0.25%, not ie 11, not op_mini all, not dead'
     },
     'postcss-custom-media': {},
+    "tailwindcss/nesting": {},
+    tailwindcss: {},
     autoprefixer: {}
   }
 };
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+     './src/**/*.html',
+     './src/**/*.erb',
+     './src/**/*.slim',
+     './src/**/*.js',
+   ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
